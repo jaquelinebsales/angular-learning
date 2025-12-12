4801b7b9e41c7dd6e24958d3b037ee8134a4fb87
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-12-12 17:55:19 -0300

    jhdfsk

--- jornada-milhas/src/app/pages/home/home.component.ts
+++ jornada-milhas/src/app/pages/home/home.component.ts
@@ -3,13 +3,15 @@ import { BannerComponent } from '../../shared/banner/banner.component';
 import { ContainerComponent } from '../../shared/container/container.component';
 import { CardBuscaComponent } from '../../shared/card-busca/card-busca.component';
 import { CardDepoimentosComponent } from '../../shared/card-depoimentos/card-depoimentos.component';
+import { FormBuscaComponent } from '../../shared/form-busca/form-busca.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [BannerComponent,
             ContainerComponent,
             CardBuscaComponent,
-            CardDepoimentosComponent],
+            CardDepoimentosComponent,
+            FormBuscaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
