58cce07159e1ce82ee2e5c5915aff103eda28494
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-12-18 11:37:58 -0300

    jornada milhas finalizado

--- jornada-milhas/src/app/shared/modal/modal.component.ts
+++ jornada-milhas/src/app/shared/modal/modal.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatChipsModule } from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [ MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule,MatChipsModule, MatIconModule, MatDividerModule ],
+  providers: [ MatDialog ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.scss'
+  styleUrl: './modal.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 
 export class ModalComponent {
-
+  readonly bestBoys: string[] = ['Executiva', 'Econômica'];
 }
 
